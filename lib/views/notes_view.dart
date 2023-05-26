cdbe00088e02f10810068c1e821d3acf7f06f9d3
bhuvan <jbhuvanesh16@gmail.com>
2023-05-26 13:12:58 +0530

    Added note service to Notes_view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notenow/constants/routes.dart';
 import 'package:notenow/enums/menu_action.dart';
 import 'package:notenow/services/auth/auth_service.dart';
+import 'package:notenow/services/crud/notes_service.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -11,6 +12,22 @@ class NotesView extends StatefulWidget {
 }
 
 class _NotesViewState extends State<NotesView> {
+
+  late final NotesService _notesService;
+  String get userEmail => AuthService.firebase().currentUser!.email!;
+
+  @override
+  void initState() {
+    _notesService = NotesService();
+      super.initState();
+  }
+
+  @override
+  void dispose() {
+    _notesService.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,11 +61,31 @@ class _NotesViewState extends State<NotesView> {
               },
             ),
           ]),
+          body: FutureBuilder(
+            future: _notesService.getOrCreateUser(email: userEmail),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {                
+                case ConnectionState.done:
+                  return StreamBuilder(
+                    stream: _notesService.allNotes,
+                    builder: (context, snapshot) {
+                      switch (snapshot.connectionState){
+                        case ConnectionState.waiting:
+                          return const Text("Waiting for all notes");
+                        default:
+                          return const CircularProgressIndicator();
+                      }  
+                    },
+                  );
+                default:
+                  return const CircularProgressIndicator();
+              }
+
+            },
+          )
     );
   }
 }
-
-
 
 Future<bool> showLogOutDialog(BuildContext context) {
   return showDialog<bool>(
